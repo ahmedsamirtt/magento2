ba34290a2734e383e341a5403e9a9300267ee34f
test <test@9750-5007-6974>
2024-05-08 12:22:04 +0300

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -2858,9 +2858,9 @@
             ),
         ),
         'twentytoo/autotag' => array(
-            'pretty_version' => '1.1.90',
-            'version' => '1.1.90.0',
-            'reference' => '665312f5497496637eafce644bef99f795ee151b',
+            'pretty_version' => '1.1.91',
+            'version' => '1.1.91.0',
+            'reference' => 'f986721c34febf3f8c9f770a88b5eb93c4a90c9b',
             'type' => 'magento2-module',
             'install_path' => __DIR__ . '/../twentytoo/autotag',
             'aliases' => array(),
