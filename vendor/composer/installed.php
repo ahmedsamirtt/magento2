b7c67784a05cd525481e1fb1ed8f0d7748ae32f4
test <test@9750-5007-6974>
2024-05-14 14:39:59 +0300

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -2867,9 +2867,9 @@
             'dev_requirement' => false,
         ),
         'twentytoo/textsearch' => array(
-            'pretty_version' => '1.0.3',
-            'version' => '1.0.3.0',
-            'reference' => 'bee5c37a9607f5b1a08bd76a76850ab493b4ec86',
+            'pretty_version' => '1.0.4',
+            'version' => '1.0.4.0',
+            'reference' => 'cfcdfff4d46b9622e2535d7afa2486bd27ee2de3',
             'type' => 'magento2-module',
             'install_path' => __DIR__ . '/../twentytoo/textsearch',
             'aliases' => array(),
