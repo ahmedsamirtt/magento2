623395146fd2d399043bbcd508d4d6061f0db9c4
test <test@9750-5007-6974>
2024-05-29 11:48:56 +0300

    update

--- vendor/twentytoo/textsearch/Observer/SearchQueryObserver.php
+++ vendor/twentytoo/textsearch/Observer/SearchQueryObserver.php
@@ -7,21 +7,25 @@ use Magento\Framework\Event\ObserverInterface;
 use Psr\Log\LoggerInterface;
 use TwentyToo\TextSearch\Service\ApiService;
 use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Registry;
 
 class SearchQueryObserver implements ObserverInterface
 {
     protected $logger;
     protected $apiService;
     protected $session;
+    protected $registry;
 
     public function __construct(
         LoggerInterface $logger,
         ApiService $apiService,
-        SessionManagerInterface $session
+        SessionManagerInterface $session,
+        Registry $registry
     ) {
         $this->logger = $logger;
         $this->apiService = $apiService;
         $this->session = $session;
+        $this->registry = $registry;
     }
 
     public function execute(Observer $observer)
@@ -31,13 +35,18 @@ class SearchQueryObserver implements ObserverInterface
         $query = $observer->getControllerAction();
         $queryText = $query->getRequest()->getParam('q');
         $this->logger->info('Search query: ' . $queryText);
+
         $productIds = $this->apiService->getProductIdsFromApi($queryText);
-        $this->logger->info('Service Products: ' . json_encode($productIds));
+        $this->logger->info('Service Products Observer: ' . json_encode($productIds));
+
 
         if (!empty($productIds)) {
             $this->session->setCustomProductIds($productIds);
             $this->session->setSearchQuery($queryText);
             $this->logger->info('Session product IDs and query set.');
+
+            // Register custom data in the registry
+            $this->registry->register('custom_data_key', $productIds);
         }
     }
 }
