b0a89df63b61b07fb7ceb45818a22bc5319c24b7
test <test@9750-5007-6974>
2024-05-29 13:44:29 +0300

    update

--- vendor/twentytoo/textsearch/Plugin/SearchResultPlugin.php
+++ vendor/twentytoo/textsearch/Plugin/SearchResultPlugin.php
@@ -7,6 +7,7 @@ use TwentyToo\TextSearch\Service\ApiService;
 use Psr\Log\LoggerInterface;
 use Magento\Framework\Session\SessionManagerInterface;
 use Magento\Framework\Registry;
+use Magento\Search\Model\QueryFactory;
 
 class SearchResultPlugin
 {
@@ -14,39 +15,48 @@ class SearchResultPlugin
     protected $logger;
     protected $session;
     protected $registry;
+    protected $queryFactory;
 
     public function __construct(
         ApiService $apiService,
         LoggerInterface $logger,
         SessionManagerInterface $session,
-        Registry $registry
+        Registry $registry,
+        QueryFactory $queryFactory
     ) {
         $this->apiService = $apiService;
         $this->logger = $logger;
         $this->session = $session;
         $this->registry = $registry;
+        $this->queryFactory = $queryFactory;
     }
 
     public function afterLoad(Collection $subject, Collection $result)
     {
-        $this->logger->info('SearchQueryPlugin: Plugin executed.');
-        $queryText = $subject->getSearchQueryText();
-        $this->logger->info('Search query Plugin Test: ' . $queryText);
+        try {
+            $this->logger->info('SearchQueryPlugin: Plugin executed.');
 
-        $productIds = $this->apiService->getProductIdsFromApi($queryText);
-        $this->logger->info('Service Products Plugin: ' . json_encode($productIds));
+            $searchQuery = $this->queryFactory->get();
+            $queryText = $searchQuery->getQueryText();
+            $this->logger->info('Search query Plugin: ' . $queryText);
 
-        if (!empty($productIds)) {
-            // Filter the collection to include only the product IDs returned from the API
-            $result->addFieldToFilter('entity_id', ['in' => [1,1,1,1,1]]);
-            $this->logger->info('Search collection updated with new product IDs.');
+            $productIds = $this->apiService->getProductIdsFromApi($queryText);
+            $this->logger->info('Service Products Plugin: ' . json_encode($productIds));
 
-            // Store the product IDs and search query in the session
-            $this->session->setCustomProductIds($productIds);
-            $this->session->setSearchQuery($queryText);
-            $this->registry->register('custom_data_key', $productIds);
-        } else {
-            $this->logger->info('No product IDs fetched from API.');
+            if (!empty($productIds)) {
+                // Filter the collection to include only the product IDs returned from the API
+                $result->addFieldToFilter('entity_id', ['in' => [1,1,1,1]]);
+                $this->logger->info('Search collection updated with new product IDs.');
+
+                // Store the product IDs and search query in the session
+                $this->session->setCustomProductIds($productIds);
+                $this->session->setSearchQuery($queryText);
+                $this->registry->register('custom_data_key', $productIds);
+            } else {
+                $this->logger->info('No product IDs fetched from API.');
+            }
+        } catch (\Exception $e) {
+            $this->logger->error('Error in SearchResultPlugin: ' . $e->getMessage());
         }
 
         return $result;
