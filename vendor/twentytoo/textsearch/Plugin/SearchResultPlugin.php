d648d68e5c1c7e09f0ac0924d513bc8f32157fff
test <test@9750-5007-6974>
2024-05-30 14:50:50 +0300

    update

--- vendor/twentytoo/textsearch/Plugin/SearchResultPlugin.php
+++ vendor/twentytoo/textsearch/Plugin/SearchResultPlugin.php
@@ -31,8 +31,12 @@ class SearchResultPlugin
         $this->queryFactory = $queryFactory;
     }
 
-    public function afterLoad(Collection $subject, Collection $result)
-    {
+    public function aroundLoad(
+        Collection $subject,
+        callable $proceed
+    ) {
+        $result = $proceed();
+        
         try {
             $this->logger->info('SearchQueryPlugin: Plugin executed.');
 
@@ -44,19 +48,23 @@ class SearchResultPlugin
             $productIds = $this->apiService->getProductIdsFromApi($queryText);
             $this->logger->info('Dynamic product IDs fetched from API: ' . json_encode($productIds));
 
-            // Use static product IDs [1, 1, 1, 1]
+            // Use static product IDs
             $staticProductIds = [2];
             $this->logger->info('Using static product IDs: ' . implode(', ', $staticProductIds));
 
-            $this->logger->info('Before filtering - Total items: ' . $result->getSize());
-            $staticProductCollection = $this->productCollectionFactory->create()
-            ->addAttributeToSelect('*')
-            ->addFieldToFilter('entity_id', ['in' => $staticProductIds]);
+            // Clear the current result items and replace with static product IDs
+            $subject->clear();
+            $this->logger->info('Current result items cleared.');
 
-            $this->logger->info("Load static product collection");
-            // Clear the current result items
-            $result->clear();
-            $this->logger->info("Clear current");
+            $select = $subject->getSelect();
+            $this->logger->info('Current select statement: ' . $select->__toString());
+
+            $select->where('e.entity_id IN (?)', $staticProductIds);
+            $this->logger->info('Select statement updated with static product IDs.');
+
+            $subject->load();
+            $this->logger->info('Search collection loaded with updated select statement.');
+
             $this->logger->info('Search collection updated with static product IDs.');
 
             // Optionally, store the static product IDs and search query in the session or registry
