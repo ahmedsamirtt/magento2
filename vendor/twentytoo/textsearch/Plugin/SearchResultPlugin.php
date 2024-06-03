e788372529ec47d0758ce3d97ebbeb9eed9d24bf
test <test@9750-5007-6974>
2024-06-03 12:15:05 +0300

    update

--- vendor/twentytoo/textsearch/Plugin/SearchResultPlugin.php
+++ vendor/twentytoo/textsearch/Plugin/SearchResultPlugin.php
@@ -4,14 +4,17 @@ namespace TwentyToo\TextSearch\Plugin;
 
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Framework\Api\Search\SearchResultInterface;
+use Psr\Log\LoggerInterface;
 
 class SearchResultPlugin
 {
     protected $productCollectionFactory;
+    protected $logger;
 
-    public function __construct(CollectionFactory $productCollectionFactory)
+    public function __construct(CollectionFactory $productCollectionFactory, LoggerInterface $logger)
     {
         $this->productCollectionFactory = $productCollectionFactory;
+        $this->logger = $logger;
     }
 
     public function aroundGetList(
@@ -19,8 +22,8 @@ class SearchResultPlugin
         callable $proceed,
         \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
     ) {
-        // Skip the original method call to bypass the actual search
-        // $result = $proceed($searchCriteria);
+        // Log the search query
+        $this->logger->info('Search query intercepted, replacing with specific product IDs.');
 
         // Load the specific products you want to always return
         $specificProductIds = [1, 3];
@@ -28,11 +31,17 @@ class SearchResultPlugin
             ->addAttributeToSelect('*')
             ->addAttributeToFilter('entity_id', ['in' => $specificProductIds]);
 
+        // Log the number of products found
+        $this->logger->info('Loaded specific products: ' . implode(', ', $specificProductIds));
+
         // Create a new search result interface to return
         /** @var SearchResultInterface $result */
         $result = $subject->getList($searchCriteria);
         $result->setItems($productCollection->getItems());
         $result->setTotalCount(count($specificProductIds));
+
+        // Log the final result count
+        $this->logger->info('Returning modified search results with ' . count($specificProductIds) . ' products.');
 
         return $result;
     }
